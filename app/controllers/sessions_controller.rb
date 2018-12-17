e3a0177a6a27f4f719a442fbf61288d4aa3398fa
ngovannghia1997bg <ngovannghia1997bg@gmail.com>
2018-12-17 15:57:13 +0700

    chuong 10 updating-users

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,11 +2,11 @@ class SessionsController < ApplicationController
   def new; end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
+    user = User.find_by email: params[:session][:email].downcase
+    if user&.authenticate(params[:session][:password])
       log_in user
       params[:session][:remember_me] == "1" ? remember(user) : forget(user)
-      redirect_to user
+      redirect_back_or user
     else
       flash.now[:danger] = t ".invalid_email_password"
       render :new
