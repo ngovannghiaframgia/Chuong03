ba4e9fea9157bb50df3a256273da3a46d7db2d13
ngovannghia1997bg <ngovannghia1997bg@gmail.com>
2018-12-17 15:57:13 +0700

    chuong 08 basic-login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
 
   before_action :set_locale
 
+  include SessionsHelper
+
   def set_locale
     locale = params[:locale].to_s.strip.to_sym
     I18n.locale = I18n.available_locales.include?(locale) ? locale : I18n.default_locale
